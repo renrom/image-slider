fc2a49e5471826f4149c6f4514cc90173f000337
¨rene@instinkt.nl¨ <rene@instinkt.nl>
2024-03-05 19:29:29 +0100

    changed to webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,126 @@
-import '../css/styles.css'
+import image1  from '../assets/images/1.jpg';
+import image2  from '../assets/images/2.jpg';
+import image3  from '../assets/images/3.jpg';
+
+import '../css/styles.css';
+
+
+const locationImages = [
+    image1,
+    image2,
+    image3,
+]
+
+
+
+const imagePlaceHolder = document.querySelector('.image-container');
+const imageSelectLeft = document.querySelector('.image-left');
+const imageSelectRight = document.querySelector('.image-right')
+
+const totalImages = locationImages.length-1;
+let selectedImage = 0;
+
+
+function hideImage(image) {
+    const allImages = document.getElementsByClassName('image');
+    allImages[selectedImage].style.visibility = 'hidden';
+    allImages[selectedImage].style.opacity = '0';
+
+}
+
+function showImage(image) {
+    const allImages = document.getElementsByClassName('image');
+    allImages[image].style.visibility = 'visible';
+    allImages[image].style.opacity = '1';
+
+}
+
+function imageToTheLeft() {
+    hideImage(selectedImage);
+
+    
+    if (selectedImage === 0) {
+        selectedImage = totalImages;
+    } else {
+        selectedImage--;
+    }
+    showImage(selectedImage);
+}
+
+function imageToTheRight() {
+    
+    hideImage(selectedImage);
+    if (selectedImage === totalImages) {
+        selectedImage = 0;
+    } else {
+        selectedImage++;
+    }
+
+    showImage(selectedImage);
+}
+
+imageSelectLeft.addEventListener('click', () => {
+
+    imageToTheLeft();
+});
+
+imageSelectRight.addEventListener('click', () => {
+    imageToTheRight();
+});
+
+
+setInterval(imageToTheRight, 5000);
+
+let dotsPlacement = document.querySelector(".dots");
+
+let currentImage = 0;
+
+locationImages.forEach(element => {
+
+    const image = document.createElement('img');
+    image.className = 'image';
+    image.src = element;
+    image.style.width = '300px';
+    image.style.visibility = 'hidden';
+    imagePlaceHolder.appendChild(image);
+
+    const dotSlider = document.createElement('span');
+    dotSlider.textContent = ' O ';
+    dotSlider.style.cursor = 'pointer';
+
+    const fixedCurrentImage = currentImage;
+
+    dotSlider.addEventListener("click", () => {
+
+        
+        console.log(fixedCurrentImage);
+        
+        hideImage(selectedImage);      
+        showImage(fixedCurrentImage);
+
+        
+
+
+        if (currentImage === totalImages) {
+            selectedImage = 0;
+        } else {
+            selectedImage = fixedCurrentImage;
+        }
+
+        
+
+
+    });
+
+    dotsPlacement.appendChild(dotSlider);
+    currentImage++;
+
+
+});
+
+
+const image = document.querySelector('.image-container').firstElementChild;
+image.style.visibility = 'visible';
+
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
+  performance: {
+    maxAssetSize: 500000,
+    maxEntrypointSize: 500000,
+    assetFilter: function(assetFilename) {
+      return !assetFilename.endsWith('.jpg')
+    },
+  },
   mode: "production",
   entry: "./src/scripts/index.js",
   output: {
@@ -10,10 +17,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [new HtmlWebpackPlugin({
-	title: 'Template Website',
-	template: './src/index.html',
-	filename: './index.html',
-
+    title: 'Template Website',
+    template: './src/index.html',
+    filename: './index.html',
+  
   })],
   devServer: {
     static: "./dist",
